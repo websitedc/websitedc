d0001097688ffd8fc7aee11bc9f7a3ab55277e00
websitedc <laurent.dekerpel@gmail.com>
2014-03-24 22:46:15 +0100

    add footer

---
+++ view/footer.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<div id="footer_container_main">
+    <ul class="ul_footer">
+        <?php
+        $pages = $translator->get_vtree();
+        foreach ($pages->vtree["navigation"] as $name => $page) {
+            echo "<li> <a href='" . $page . "'>" . $name . "</a></li>";
+        }
+        ?>
+    </ul>
+    <span id="footer_copyright">
+        © Website Design & Conception 2014
+    </span>
+</div>
