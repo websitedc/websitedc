eb93b2666e9f813dafbeef4c929bb6856d9fa1df
Fredou <fclaeys.project@gmail.com>
2014-03-24 21:00:05 +0100

    add validation

--- view/contact.php
+++ view/contact.php
@@ -5,20 +5,23 @@ To change this template file, choose Tools | Templates
 and open the template in the editor.
 -->
 <html>
-<head>
-<meta charset="ISO-8859-15">
-<meta name="keywords" content="website design, conception, creation de site internet" lang="fr">
-<title><?php translate('HOMEPAGE_TITLE'); ?></title>
-<link href="view/css/common.css" rel="stylesheet" type="text/css" media="all">
-<link href="view/css/homepage.css" rel="stylesheet" type="text/css" media="all">
-<style type="text/css"></style>
+    <head>
+        <meta charset="ISO-8859-15">
+        <meta name="keywords" content="website design, conception, creation de site internet" lang="fr">
+        <title><?php translate('HOMEPAGE_TITLE'); ?></title>
+        <link href="view/css/common.css" rel="stylesheet" type="text/css" media="all">
+        <link href="view/css/homepage.css" rel="stylesheet" type="text/css" media="all">
+        <style type="text/css"></style>
 
-</head>
-<body>
-<?php
-    include_once 'view/header.php';
-    ?>
-Bonjour, <?php echo htmlspecialchars($_POST['nom']); ?>.
-Tu as <?php echo (int) $_POST['age']; ?> ans.
-</body>
+    </head>
+    <body>
+        <form action="action.php" method="post">
+            <p>Votre nom : <input type="text" name="nom"/></p>
+            <p>Votre prénom : <input type="text" name="prenom"/></p>
+            <p>Votre email : <input type="email" name="email"/></p>
+            <p>Votre Telephone = <input type="tel" name="tel"/></p>
+            <p>Votre entreprise : <input type="text" name="entreprise"/></p>
+            <p><input type="submit" value="OK"></p>
+        </form>
+    </body>
 </html>
