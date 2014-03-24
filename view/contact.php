24bb470ba143460f55e94e851d3f41a7a65249bb
Fredou <fclaeys.project@gmail.com>
2014-03-24 20:45:10 +0100

    change contact

--- view/contact.php
+++ view/contact.php
@@ -5,13 +5,20 @@ To change this template file, choose Tools | Templates
 and open the template in the editor.
 -->
 <html>
-    <head>
-        <meta charset="ISO-8859-15">
-        <title><?php translate("TITLE")?></title>
-    </head>
-    <body>
-        <?php
-        include_once 'view/header.php';
-        ?>
-    </body>
+<head>
+<meta charset="ISO-8859-15">
+<meta name="keywords" content="website design, conception, creation de site internet" lang="fr">
+<title><?php translate('HOMEPAGE_TITLE'); ?></title>
+<link href="view/css/common.css" rel="stylesheet" type="text/css" media="all">
+<link href="view/css/homepage.css" rel="stylesheet" type="text/css" media="all">
+<style type="text/css"></style>
+
+</head>
+<body>
+<?php
+    include_once 'view/header.php';
+    ?>
+Bonjour, <?php echo htmlspecialchars($_POST['nom']); ?>.
+Tu as <?php echo (int) $_POST['age']; ?> ans.
+</body>
 </html>
